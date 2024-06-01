4c747b32dc42df17e765ba48eb262d4d1966f151
71405245 <71405245@continental.edu.pe>
2024-05-31 20:01:50 -0500

    Actualizado

--- test/Test_gestor_tareas.py
+++ test/Test_gestor_tareas.py
@@ -27,3 +27,28 @@ class TestGestorTareas(unittest.TestCase):
   def test_ver_tarea_sin_titulo(self):
    with self.assertRaises(ValueError):
     self.gestor.ver_tarea("", "Descripción")
+
+
+  def test_marcar_tarea(self):
+   self.gestor.marcar_tarea("Tarea en ejecucion", "Descripción de la tarea ")
+
+  self.assertEqual(len(self.gestor.tareas), 1)
+  self.assertEqual(self.gestor.tareas[0].titulo, "Tarea Completada")
+  self.assertEqual(self.gestor.tareas[0].descripcion, "Descripción de la tarea ")
+
+  def test_marcar_tarea_sin_titulo(self):
+   with self.assertRaises(ValueError):
+    self.gestor.marcar_tarea("", "Tarea completa")
+
+      def test_eliminar_tarea(self):
+     self.gestor.eliminar_tarea("Tarea en ejecucion", "Descripción de la tarea ")
+
+    self.assertEqual(len(self.gestor.tareas), 1)
+    self.assertEqual(self.gestor.tareas[0].titulo, "Tarea eliminada")
+    self.assertEqual(self.gestor.tareas[0].descripcion, "Descripción de la tarea ")
+
+    def test_eliminar_tarea_sin_titulo(self):
+     with self.assertRaises(ValueError):
+      self.gestor.eliminar_tarea("", "Tarea eliminada")
+
+
